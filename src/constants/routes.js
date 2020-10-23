0df74b85ec57846c2ce513dc0896a832b7969d3f
TomekSkoczylas <tomasz.m.skoczylas@gmail.com>
2020-10-23 18:14:14 +0200

    Dodatnie ścieżek do routera

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,7 @@
-export const LANDING = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/home';
+export const LOGIN = '/';
+export const MAIN = '/main';
+export const SIGNUP = '/signup';
+export const PASS_FORGET = '/pass-forget';
 export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
-export const PASSWORD_FORGET = '/pw-forget';
+
+
--- src/layouts/App/index.js
+++ src/layouts/App/index.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+
+import AccountPage from "../Account";
+import LogInPage from "../LogIn";
+import MainPage from "../Main";
+import PassForgetPage from "../PassForget";
+import SignUpPage from "../SignUp";
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
+    <Router>
+        <div>
+            <Route exact path={ROUTES.LOGIN} component={LogInPage}/>
+            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+            <Route path={ROUTES.MAIN} component={MainPage} />
+            <Route path={ROUTES.PASS_FORGET} component={PassForgetPage} />
+            <Route path={ROUTES.SIGNUP} component={SignUpPage} />
+        </div>
+    </Router>
+
 );
 
 export default App;
