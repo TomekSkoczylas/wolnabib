dc049f3a5944353160890f5e59e195cd4fe0426d
TomekSkoczylas <tomasz.m.skoczylas@gmail.com>
2020-10-23 18:11:39 +0200

    Dodanie Firebase II

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './layouts/App';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={new Firebase()}>
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
