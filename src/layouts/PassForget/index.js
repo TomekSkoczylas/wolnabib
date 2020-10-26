cd9370c5ebeb29c2dfc6418d5b1b80c0bf13aef2
TomekSkoczylas <tomasz.m.skoczylas@gmail.com>
2020-10-26 14:06:43 +0100

    Uzupełnienia

--- src/layouts/PassForget/index.js
+++ src/layouts/PassForget/index.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+
+import { withFirebase } from "../../components/Firebase";
+import * as ROUTES from '../../constants/routes';
 
 const PassForgetPage = () => (
     <div>
         <h1>Zapomniałem Hasła</h1>
+        <PassForgetForm />
     </div>
 );
+
+const INITIAL_STATE = {
+    email: '',
+    error: null,
+}
+
+const PassForgetForm = (props) => {
+    const [mail, setMail] = useState({...INITIAL_STATE});
+
+    const onSubmit = event => {
+
+    }
+
+    const onChange = event => {
+
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                name="email"
+                value={mail.email}
+                onChange={onChange}
+                type="text"
+                placeholder="Podaj adres e-mail"
+            />
+            <button disabled={isInvalid} type="submit">
+                Zresetuj moje hasło
+            </button>
+
+
+        </form>
+
+    )
+
+
+
+}
+
 
 export default PassForgetPage;
 
